f9d0ca5ac8f6107e0c1bd2f11c7b207a45d33292
SaurabhRajput15 <SaurabhRajput15>
2024-09-12 11:52:24 +0530

    namaste-react code

---
+++ App.js
@@ -0,0 +1,9 @@
+let heading = React.createElement(
+    "h1", 
+    {id:"header"}, 
+    "Hello World from React!"
+)
+        
+let root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
